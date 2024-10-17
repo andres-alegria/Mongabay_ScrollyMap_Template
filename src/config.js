977efa653888a200b028b696c800c4e46b40bc9a
Willie Shubert <willie.pictograms@gmail.com>
2024-10-17 13:27:40 -0700

    Images uploaded

--- src/config.js
+++ src/config.js
@@ -64,19 +64,19 @@ export default {
                layer: 'airstrips-10224-dozgmq',
                opacity: 1,
            },
-           {
-               layer: 'concesiones-forestales-10-432-dulhn7 copy 2',
-               opacity: 0,
-           },
-           {
-               layer: 'concesiones-forestales-10-432-dulhn7 copy 1',
-               opacity: 0,
-           },
-           {
-               layer: 'coca-crops-density-20241002t1-55l40t',
-               opacity: 0,
-
-           },
+           // {
+           //     layer: 'concesiones-forestales-10-432-dulhn7 copy 2',
+           //     opacity: 0,
+           // },
+           // {
+           //     layer: 'concesiones-forestales-10-432-dulhn7 copy 1',
+           //     opacity: 0,
+           // },
+           // {
+           //     layer: 'coca-crops-density-20241002t1-55l40t',
+           //     opacity: 0,
+           //
+           // },
         ],
         onChapterExit: [
              {
@@ -91,7 +91,14 @@ export default {
         alignment: 'right',
         hidden: false,
         title: 'Narcopistas',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'Pist069_PleiadesNeo2022-07-27.jpg',
+                position: 'top',
+                title: 'Pista 24',
+                author: 'SPOT'
+              }
+            ],
         description: 'La verificación periodística de cada una de ellas, con fuentes oficiales y locales, fue clave para confirmar que 67 de las 76 narcopistas están distribuidas entre Ucayali (45), Huánuco (17) y Pasco (5), y sirven hoy al narcotráfico.',
         location: {
             center: [-74.549, -9.575],
@@ -133,7 +140,6 @@ export default {
         alignment: 'left',
         hidden: false,
         title: 'Los vuelos de la muerte',
-        image: './path/to/image/source.png',
         description: 'Se trata de tres regiones que configuran un triángulo geográfico en el que al menos 15 líderes indígenas han sido asesinados desde la pandemia, en 2020. Desde esos territorios parten la mayor cantidad de vuelos cargados de cocaína, como confirmaron fuentes de la Dirección Antidrogas de la Policía Nacional del Perú.',
         location: {
             center: [-74.549, -9.575],
@@ -162,7 +168,14 @@ export default {
         alignment: 'fully',
         hidden: false,
         title: 'Atalaya',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'Pist018_SPOT2020-08-01.jpg',
+                position: 'top',
+                title: 'Pista 18',
+                author: 'SPOT Image'
+              }
+            ],
         description: 'Algunos de los tajos abiertos en ese triángulo de ilegalidad han tomado paisajes completos. Sólo en la provincia de Atalaya, en la región Ucayali, se detectaron 32 pistas.',
         location: {
             center: [-73.413, -10.724],
@@ -213,14 +226,27 @@ export default {
 
           }
         ],
-        onChapterExit: []
+        onChapterExit: [
+                  {
+                      layer: 'coca-crops-density-20241002t1-55l40t',
+                      opacity: 0,
+
+                  }
+                ]
     },
     {
         id: 'sixth-chapter',
         alignment: 'left',
         hidden: false,
         title: 'Cercadas por la ilegalidad',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'OverviewPist058-059-065-082-084_SPOT2022-07-30.jpg',
+                position: 'top',
+                title: 'Múltiples puntos de aterrizaje',
+                author: 'SPOT IMAGE'
+              }
+            ],
         description: 'Siguiendo la información satelital, periodistas de Mongabay Latam llegaron a dos comunidades, ubicadas entre las regiones de Huánuco y Pasco, que soportan juntas la presencia de 15 pistas de aterrizaje clandestinas en sus territorios.',
         location: {
             center: [-74.927, -9.790],
@@ -253,7 +279,14 @@ export default {
         alignment: 'right',
         hidden: false,
         title: 'Reservas indígenas bajo presión',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'Pist014_SPOT2022-07-05.jpg',
+                position: 'top',
+                title: 'Pista 14',
+                author: 'SPOT Image'
+              }
+            ],
         description: 'Dentro de la Reserva Indígena Kakataibo Norte y Sur es posible ver al menos 02 tajos ilegales. Además, al menos 4 reservas indígenas para pueblos en aislamiento y contacto inicial son afectadas por la presencia de 6 aeródromos ilegales. Aunque estos territorios deberían ser intangibles por ley, en la práctica no lo son.',
         location: {
             center: [-75.3976425, -9.367474578],
@@ -291,7 +324,14 @@ export default {
         alignment: 'right',
         hidden: false,
         title: 'Concesiones forestales',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'DSC_0723-1.jpg',
+                position: 'top',
+                title: 'la madera transita los ríos',
+                author: 'Santiago Romaní / Mongabay Latam'
+              }
+            ],
         description: 'Las narcopistas se han instalado incluso dentro de 9 concesiones forestales que están destinadas al aprovechamiento sostenible del bosque. El algoritmo detectó en total 10 trazos.',
         location: {
             center: [-74.549, -9.575],
@@ -332,7 +372,14 @@ export default {
         alignment: 'right',
         hidden: false,
         title: 'Forestal Mendoza',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'Pist024-SPOT2023-10-02v2.jpg',
+                position: 'top',
+                title: 'Pista 24',
+                author: 'SPOT'
+              }
+            ],
         description: 'Una de las concesiones que presenta pistas clandestinas es administrada por la empresa Forestal Mendoza, que tiene como representante legal a Francisco de Asís Mendoza de Souza, actual alcalde provincial de Atalaya, que acumula investigaciones por usurpación, peculado y lavado de activos. El algoritmo detectó dentro de la concesión dos pistas de aterrizaje ilegales: una abierta en junio del 2018 y la otra en el mismo mes del 2020. ',
         location: {
             center: [-73.4800647201553, -9.46968987568065],
@@ -373,7 +420,14 @@ export default {
         alignment: 'right',
         hidden: false,
         title: 'Ríos y vías',
-        image: './path/to/image/source.png',
+        images: [
+              {
+                src: 'Pist021_PleiadesNeo2022-09-27.jpg',
+                position: 'top',
+                title: 'Pista 18',
+                author: 'SPOT Image'
+              }
+            ],
         description: 'Un hallazgo clave de la investigación es que 52 de las 67 narcopistas situadas en el triángulo de la muerte que conforman Ucayali, Huánuco y Pasco —donde han asesinado a la mayoría de líderes indígenas— están a un kilómetro de ríos y vías, lo que las convierte en puntos estratégicos para el traslado de los cargamentos de droga.',
         location: {
             center: [-74.549, -9.575],
