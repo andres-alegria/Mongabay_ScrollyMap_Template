3778f44ce1f2908b12ba2825eed1aae96c5ec3fc
Willie Shubert <willie.pictograms@gmail.com>
2024-10-20 15:40:24 -0700

    English translation and sources complete

--- src/config.js
+++ src/config.js
@@ -6,19 +6,19 @@ export default {
   showMarkers: false,
   theme: 'mongabay',
   intro: {
-    title: 'Narcopistas',
-    subtitle: 'Mongabay Latam y Earth Genome detectaron 67 pistas de aterrizaje clandestinas utilizadas para el transporte de droga en las regiones peruanas de Ucayali, Huánuco y Pasco.',
+    title: 'Narco airstrips',
+    subtitle: 'Using the Earth Index, an AI algorithm, Mongabay detected 76 clandestine landing strips hidden in the Peruvian Amazon.',
     date: 'Oct. 24 2024',
     social: [
       {
         name: 'twitter',
         src: 'twitter.svg',
-        href: 'ADD TWITTER LINK'
+        href: 'https://x.com/MongabayLatam'
       },
       {
         name: 'facebook',
         src: 'facebook.svg',
-        href: 'ADD FACEBOOK LINK'
+        href: 'https://www.facebook.com/MongabayLatam/'
       }
     ]
   },
@@ -43,17 +43,17 @@ export default {
         id: 'first chapter',
         alignment: 'left',
         hidden: false,
-        title: 'Pistas aéreas detectadas',
-        description: 'El algoritmo de inteligencia artificial detectó 76 pistas de aterrizaje clandestinas ocultas en la selva peruana.',
+        title: 'Airstips detected',
+        description: 'Using the Earth Index, an AI algorithm, Mongabay detected 76 clandestine landing strips hidden in the Peruvian Amazon.',
         legend: [
               {
-                title: 'Pistas de aterrizaje',
+                title: 'Landing strips',
                 color: '#037760',
                 border: 'white',
                 type: 'circle'
               }
         ],
-        sources: 'Earth Genome y Mongabay',
+        sources: "National Police of Peru's Anti-Narcotics Unit (Dirandro), Earth Genome, OpenStreetMap, and Ucayali Regional Government.",
         location: {
             center: [-74.19873, -9.59581],
             zoom: 4.35,
@@ -98,23 +98,23 @@ export default {
         id: 'second-chapter',
         alignment: 'right',
         hidden: false,
-        title: 'Narcopistas',
+        title: 'Narco airstrips',
         images: [
               {
                 src: 'Pist069_PleiadesNeo2022-07-27.jpg',
                 position: 'top',
-                title: 'Pista 24',
+                title: 'Airstrip 69',
                 author: 'SPOT'
               }
             ],
-        description: 'La verificación periodística de cada una de ellas, con fuentes oficiales y locales, fue clave para confirmar que 67 de las 76 narcopistas están distribuidas entre Ucayali (45), Huánuco (17) y Pasco (5), y sirven hoy al narcotráfico.',
+        description: 'Journalistic verification of each aistrip involved cross referencing results from Earth Index with government data, open source information and local sources. This investigation confirms that 67 of the 76 drug runways are distributed between Ucayali (45), Huánuco (17) and Pasco (5), and today serve drug trafficking.',
         legend: [
                 {
-                  title: 'Densidad del cultivo de coca',
+                  title: 'Coca crop density in 2023',
                   color: '#f1e732'
                 }
               ],
-        sources: 'ADD SOURCE',
+        sources: "Peru's National Commission for Development and Life without Drugs (DEVIDA)",
         location: {
             center: [-74.549, -9.575],
             zoom: 8,
@@ -154,8 +154,8 @@ export default {
         id: 'third-chapter',
         alignment: 'left',
         hidden: false,
-        title: 'Los vuelos de la muerte',
-        description: 'Se trata de tres regiones que configuran un triángulo geográfico en el que al menos 15 líderes indígenas han sido asesinados desde la pandemia, en 2020. Desde esos territorios parten la mayor cantidad de vuelos cargados de cocaína, como confirmaron fuentes de la Dirección Antidrogas de la Policía Nacional del Perú.',
+        title: 'Deadly flights',
+        description: 'In these three regions, at least 15 Indigenous leaders have been murdered since the pandemic began in 2020. The area is also home to the largest number of flights shipping cocaine, according to sources from the Anti-Drug Directorate of the National Police of Peru.',
         legend: [
                 {
                   title: 'Territorios Indigenas',
@@ -163,7 +163,7 @@ export default {
                   pattern: 'LINEPATTERN'
                 }
               ],
-        sources: 'ADD SOURCE',
+        sources: 'Global Forest Watch and Instituto del Bien Común (IBC)',
         location: {
             center: [-74.549, -9.575],
             zoom: 8,
@@ -190,23 +190,23 @@ export default {
         id: 'fourth-chapter',
         alignment: 'fully',
         hidden: false,
-        title: 'Atalaya',
+        title: 'Atalaya Province',
         images: [
               {
                 src: 'Pist018_SPOT2020-08-01.jpg',
                 position: 'top',
-                title: 'Pista 18',
+                title: 'Airstrip 18',
                 author: 'SPOT Image'
               }
             ],
-        description: 'Algunos de los tajos abiertos en ese triángulo de ilegalidad han tomado paisajes completos. Sólo en la provincia de Atalaya, en la región Ucayali, se detectaron 32 pistas.',
+        description: 'Illegal airstips have taken over entire landscapes in this corner of the Amazon Rainforest. In the province of Atalaya, which is part of the Ucayali region, we detected 32 airstrips.',
         legend: [
                 {
                   title: 'Deforestación',
                   color: '#ccefb8',
                 }
               ],
-        sources: 'ADD SOURCE',
+        sources: 'Global Forest Watch',
         location: {
             center: [-73.413, -10.724],
             zoom: 9.83,
@@ -233,8 +233,8 @@ export default {
         id: 'fifth-chapter',
         alignment: 'left',
         hidden: false,
-        title: 'Territorios indígenas',
-        description: 'Lo crítico es que 30 de las 67 narcopistas detectadas por el algoritmo están dentro de territorios indígenas y 26 alrededor.',
+        title: 'Indigenous territories',
+        description: 'Crucially, 30 of the 67 narco airstrops were detected within Indigenous territories and 26 more are located near them.',
         location: {
             center: [-74.549, -9.575],
             zoom: 8,
@@ -267,16 +267,16 @@ export default {
         id: 'sixth-chapter',
         alignment: 'left',
         hidden: false,
-        title: 'Cercadas por la ilegalidad',
+        title: 'Surrounded by illegality',
         images: [
               {
                 src: 'OverviewPist058-059-065-082-084_SPOT2022-07-30.jpg',
                 position: 'top',
-                title: 'Múltiples puntos de aterrizaje',
+                title: 'Multiple landing strips',
                 author: 'SPOT IMAGE'
               }
             ],
-        description: 'Siguiendo la información satelital, periodistas de Mongabay Latam llegaron a dos comunidades, ubicadas entre las regiones de Huánuco y Pasco, que soportan juntas la presencia de 15 pistas de aterrizaje clandestinas en sus territorios.',
+        description: 'Following up on the satellite imagery analysis, Mongabay Latam journalists reached two communities, located between the regions of Huánuco and Pasco with 15 clandestine landing strips in their territories.',
         location: {
             center: [-74.927, -9.790],
             zoom: 11.06,
@@ -307,16 +307,16 @@ export default {
         id: 'seventh-chapter',
         alignment: 'right',
         hidden: false,
-        title: 'Reservas indígenas bajo presión',
+        title: 'Indigenous reserves under pressure',
         images: [
               {
                 src: 'Pist014_SPOT2022-07-05.jpg',
                 position: 'top',
-                title: 'Pista 14',
+                title: 'Airstrip 14',
                 author: 'SPOT Image'
               }
             ],
-        description: 'Dentro de la Reserva Indígena Kakataibo Norte y Sur es posible ver al menos 02 tajos ilegales. Además, al menos 4 reservas indígenas para pueblos en aislamiento y contacto inicial son afectadas por la presencia de 6 aeródromos ilegales. Aunque estos territorios deberían ser intangibles por ley, en la práctica no lo son.',
+        description: 'We identified at least 2 illegal airstrips in Kakataibo North and South Indigenous Reserve. In addition, six illegal airfields are affecting at least four reserves specificially designated for Indigenous peoples living in isolation and initial contact (PIACI). Although these territories should be off limits by law, they are not being protected in practice.',
         location: {
             center: [-75.3976425, -9.367474578],
             zoom: 11.06,
@@ -347,23 +347,23 @@ export default {
         id: 'eighth-chapter',
         alignment: 'fully',
         hidden: false,
-        title: 'Concesiones forestales',
+        title: 'Forestry concessions',
         images: [
               {
                 src: 'DSC_0723-1.jpg',
                 position: 'top',
-                title: 'la madera transita los ríos',
+                title: 'Timber transiting by river',
                 author: 'Santiago Romaní'
               }
             ],
-        description: 'Las narcopistas se han instalado incluso dentro de 9 concesiones forestales que están destinadas al aprovechamiento sostenible del bosque. El algoritmo detectó en total 10 trazos.',
+        description: 'Narco airstrips are present in nine forestry concessions, which are zoned for sustainable forest use. The Earth Index identified ten airstips within these kinds of concessions.',
         legend: [
                 {
-                  title: 'Concesiones forestales',
+                  title: 'Forestry concessions',
                   color: '#66f7ff'
                 }
               ],
-        sources: 'ADD SOURCE',
+        sources: "Peru's National Forest and Wildlife Service (Serfor)",
         location: {
             center: [-74.549, -9.575],
             zoom: 8,
@@ -402,16 +402,16 @@ export default {
         id: 'ninth-chapter',
         alignment: 'right',
         hidden: false,
-        title: 'Forestal Mendoza',
+        title: 'Mendoza Forestry',
         images: [
               {
                 src: 'Pist024-SPOT2023-10-02v2.jpg',
                 position: 'top',
-                title: 'Pista 24',
+                title: 'Airstrip 24',
                 author: 'SPOT'
               }
             ],
-        description: 'Una de las concesiones que presenta pistas clandestinas es administrada por la empresa Forestal Mendoza, que tiene como representante legal a Francisco de Asís Mendoza de Souza, actual alcalde provincial de Atalaya, que acumula investigaciones por usurpación, peculado y lavado de activos. El algoritmo detectó dentro de la concesión dos pistas de aterrizaje ilegales: una abierta en junio del 2018 y la otra en el mismo mes del 2020. ',
+        description: 'One of the forestry concessions with clandestine airstrips is managed by the company Forestal Mendoza, whose legal representative is Francisco de Asís Mendoza de Souza, the current provincial mayor of Atalaya. He is under investigation for usurpation, embezzlement and money laundering. The Earth Index detected two illegal airstrips within the concession: one opened in June 2018 and the other in June 2020.',
         location: {
             center: [-73.4800647201553, -9.46968987568065],
             zoom: 11.06,
@@ -450,16 +450,16 @@ export default {
         id: 'tenth-identifier',
         alignment: 'right',
         hidden: false,
-        title: 'Ríos y vías',
+        title: 'Rivers and roads',
         images: [
               {
                 src: 'Pist021_PleiadesNeo2022-09-27.jpg',
                 position: 'top',
-                title: 'Pista 18',
+                title: 'Airstrip 21',
                 author: 'SPOT Image'
               }
             ],
-        description: 'Un hallazgo clave de la investigación es que 52 de las 67 narcopistas situadas en el triángulo de la muerte que conforman Ucayali, Huánuco y Pasco —donde han asesinado a la mayoría de líderes indígenas— están a un kilómetro de ríos y vías, lo que las convierte en puntos estratégicos para el traslado de los cargamentos de droga.',
+        description: 'A key investigative finding is that 52 of the 67 narco airstrips located in Ucayali, Huánuco and Pasco — where the most Indigenous leaders have been murdered — are one kilometer from rivers and roads, suggesting these locations are strategic points for transfering drug shipments.',
         location: {
             center: [-74.549, -9.575],
             zoom: 8,
